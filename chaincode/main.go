cb8d444baf6cc8b34ca1008aefe60b1500044fe4
sumaikun <ventas.javc@gmail.com>
2020-08-11 14:25:59 -0500

    adding chaincode folder

---
+++ chaincode/main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// HeroesServiceChaincode implementation of Chaincode
+type HeroesServiceChaincode struct {
+}
+
+// Init of the chaincode
+// This function is called only one when the chaincode is instantiated.
+// So the goal is to prepare the ledger to handle future requests.
+func (t *HeroesServiceChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("########### HeroesServiceChaincode Init with changes ###########")
+
+	// Get the function and arguments from the request
+	function, _ := stub.GetFunctionAndParameters()
+
+	// Check if the request is the init function
+	if function != "init" {
+		return shim.Error("Unknown function call")
+	}
+
+	// Put in the ledger the key/value hello/world
+	err := stub.PutState("hello", []byte("world"))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Return a successful message
+	return shim.Success(nil)
+}
+
+// Invoke
+// All future requests named invoke will arrive here.
+func (t *HeroesServiceChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("########### HeroesServiceChaincode Invoke ###########")
+
+	// Get the function and arguments from the request
+	function, args := stub.GetFunctionAndParameters()
+
+	// Check whether it is an invoke request
+	if function != "invoke" {
+		return shim.Error("Unknown function call")
+	}
+
+	// Check whether the number of arguments is sufficient
+	if len(args) < 1 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// In order to manage multiple type of request, we will check the first argument.
+	// Here we have one possible argument: query (every query request will read in the ledger without modification)
+	if args[0] == "query" {
+		return t.query(stub, args)
+	}
+
+	// The update argument will manage all update in the ledger
+	if args[0] == "invoke" {
+		return t.invoke(stub, args)
+	}
+
+	// Check historian
+	if args[0] == "history" {
+		return t.history(stub, args)
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown action, check the first argument: " + args[0])
+}
+
+// invoke
+// Every functions that read and write in the ledger will be here
+func (t *HeroesServiceChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("########### HeroesServiceChaincode invoke ###########")
+
+	if len(args) < 2 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// Check if the ledger key is "hello" and process if it is the case. Otherwise it returns an error.
+	if args[1] == "hello" && len(args) == 3 {
+
+		// Write the new value in the ledger
+		err := stub.PutState("hello", []byte(args[2]))
+		if err != nil {
+			return shim.Error("Failed to update state of hello")
+		}
+
+		// Notify listeners that an event "eventInvoke" have been executed (check line 19 in the file invoke.go)
+		err = stub.SetEvent("eventInvoke", []byte{})
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		// Return this value in response
+		return shim.Success(nil)
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown invoke action, check the second argument.")
+}
+
+// query
+// Every readonly functions in the ledger will be here
+func (t *HeroesServiceChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("########### HeroesServiceChaincode query with cert ###########")
+
+	creatorByte, err := stub.GetCreator()
+
+	if err != nil {
+		fmt.Println("failed to get Creator: %v", err)
+	}
+
+	fmt.Println("Creator ----------------------------------")
+
+	//fmt.Println(creatorByte)
+
+	fmt.Println(string(creatorByte))
+
+	//fmt.Println("Proposal ----------------------------------")
+
+	//fmt.Println(stub.GetSignedProposal())
+
+	//fmt.Println("Transient ----------------------------------")
+
+	//fmt.Println(stub.GetTransient())
+
+	// Check whether the number of arguments is sufficient
+	if len(args) < 2 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// Like the Invoke function, we manage multiple type of query requests with the second argument.
+	// We also have only one possible argument: hello
+	if args[1] == "hello" {
+
+		// Get the state of the value matching the key hello in the ledger
+		state, err := stub.GetState("hello")
+		if err != nil {
+			return shim.Error("Failed to get state of hello")
+		}
+
+		// Return this value in response
+		return shim.Success(state)
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown query action, check the second argument.")
+}
+
+func (t *HeroesServiceChaincode) history(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("########### HeroesServiceChaincode history ###########")
+
+	// Check whether the number of arguments is sufficient
+	if len(args) < 2 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// Like the Invoke function, we manage multiple type of query requests with the second argument.
+	// We also have only one possible argument: hello
+	if args[1] == "hello" {
+
+		key := "hello"
+
+		historyIter, err := stub.GetHistoryForKey(key)
+
+		if err != nil {
+			errMsg := fmt.Sprintf("[ERROR] cannot retrieve history for key <%s>, due to %s", key, err)
+			fmt.Println(errMsg)
+			return shim.Error(errMsg)
+		}
+
+		for historyIter.HasNext() {
+			modification, err := historyIter.Next()
+			if err != nil {
+				errMsg := fmt.Sprintf("[ERROR] cannot read record modification for key %s, id <%s>, due to %s", key, err)
+				fmt.Println(errMsg)
+				return shim.Error(errMsg)
+			}
+			fmt.Println("Returning information about", string(modification.Value))
+		}
+
+		return shim.Success([]byte("done"))
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown history action, check the second argument.")
+}
+
+func main() {
+	// Start the chaincode and make it ready for futures requests
+	err := shim.Start(new(HeroesServiceChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Heroes Service chaincode: %s", err)
+	}
+}
