4bf1ba5c849d5f650a042d2f22e689316a7627ff
sumaikun <ventas.javc@gmail.com>
2020-09-04 19:21:02 -0500

    autoinstall chaincode

--- main.go
+++ main.go
@@ -100,11 +100,11 @@ func main() {
 	defer fSetup.CloseSDK()
 
 	// Install and instantiate the chaincode
-	/*err = fSetup.InstallAndInstantiateCC()
+	err = fSetup.InstallAndInstantiateCC()
 	if err != nil {
 		fmt.Printf("Unable to install and instantiate the chaincode: %v\n", err)
 		return
-	}*/
+	}
 
 	app := Application{
 		Fabric: &fSetup,
